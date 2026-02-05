8b428cf2ad2ea4d3da80f18ff94764488920b297
bakbergenmeirbek-wq <bakbergen.meirbek@narxoz.kz>
2026-02-05 08:45:06 +0500

    add main

---
+++ main.py
@@ -0,0 +1,16 @@
+#2
+def find_max(A):
+    max_val = A[0]
+    max_idx = 0
+    i = 1
+
+    while i < len(A):
+        if A[i] >= max_val:
+            max_val = A[i]
+            max_idx = i
+        i += 1
+
+    return max_val, max_idx
+print(*find_max([1, 2, 1, 2, 1]))
+
+
