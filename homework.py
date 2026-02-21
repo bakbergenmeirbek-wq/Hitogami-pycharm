b140c65c127b61190603bceef623e97189f3644e
bakbergenmeirbek-wq <bakbergen.meirbek@narxoz.kz>
2026-02-21 08:44:50 +0500

    nothing

--- homework.py
+++ homework.py
@@ -1,1 +1,12 @@
-dfjsdkfnsjkdfnjskdf
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/bakbergenmeirbek-wq/Hitogami-pycharm.git
+git push -u origin main
+
+
+
+
+
+lambda_list= lambda lst :
